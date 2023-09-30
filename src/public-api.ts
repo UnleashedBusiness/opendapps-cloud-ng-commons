034e5da19b20a25b19af0c6eeca4c872ab2f00f5
ShadowsCrypto <ceo@opendapps.cloud>
2023-09-30 21:51:19 +0300

    fixed export of compoinents

--- src/public-api.ts
+++ src/public-api.ts
@@ -5,3 +5,13 @@ export * from 'src/lib/pipe/wallet-address.pipe';
 
 export * from 'src/lib/service/beautiful-number-formatter.service';
 export * from 'src/lib/service/wallet-address-formatter.service';
+
+export * from 'src/lib/components/company-logo-holder/logo-holder.component';
+export * from 'src/lib/components/side-menu-button/side-menu-button.component';
+export * from 'src/lib/components/loading/loading.component';
+export * from 'src/lib/components/extended-amount-input/extended-amount-input.component';
+export * from 'src/lib/components/loading-overlay/loading-overlay.component';
+export * from 'src/lib/components/succesful-overlay/successful-overlay.component';
+export * from 'src/lib/components/transaction-overlay/transaction-overlay.component';
+export * from 'src/lib/components/wallet-address-wrapper/wallet-address-wrapper.component';
+export * from 'src/lib/components/wallet-connection-dialog/wallet-connection-dialog.component';

180fb9a4219be0cabb2a1090d98e9c09e17564b5
ShadowsCrypto <ceo@opendapps.cloud>
2023-09-30 21:55:01 +0300

    fixed paths in lib

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,17 +1,17 @@
-export * from 'src/lib/ng-commons.module';
+export * from 'lib/ng-commons.module';
 
-export * from 'src/lib/pipe/beautiful-number-format.pipe';
-export * from 'src/lib/pipe/wallet-address.pipe';
+export * from 'lib/pipe/beautiful-number-format.pipe';
+export * from 'lib/pipe/wallet-address.pipe';
 
-export * from 'src/lib/service/beautiful-number-formatter.service';
-export * from 'src/lib/service/wallet-address-formatter.service';
+export * from 'lib/service/beautiful-number-formatter.service';
+export * from 'lib/service/wallet-address-formatter.service';
 
-export * from 'src/lib/components/company-logo-holder/logo-holder.component';
-export * from 'src/lib/components/side-menu-button/side-menu-button.component';
-export * from 'src/lib/components/loading/loading.component';
-export * from 'src/lib/components/extended-amount-input/extended-amount-input.component';
-export * from 'src/lib/components/loading-overlay/loading-overlay.component';
-export * from 'src/lib/components/succesful-overlay/successful-overlay.component';
-export * from 'src/lib/components/transaction-overlay/transaction-overlay.component';
-export * from 'src/lib/components/wallet-address-wrapper/wallet-address-wrapper.component';
-export * from 'src/lib/components/wallet-connection-dialog/wallet-connection-dialog.component';
+export * from 'lib/components/company-logo-holder/logo-holder.component';
+export * from 'lib/components/side-menu-button/side-menu-button.component';
+export * from 'lib/components/loading/loading.component';
+export * from 'lib/components/extended-amount-input/extended-amount-input.component';
+export * from 'lib/components/loading-overlay/loading-overlay.component';
+export * from 'lib/components/succesful-overlay/successful-overlay.component';
+export * from 'lib/components/transaction-overlay/transaction-overlay.component';
+export * from 'lib/components/wallet-address-wrapper/wallet-address-wrapper.component';
+export * from 'lib/components/wallet-connection-dialog/wallet-connection-dialog.component';

d8cdc15a7682b167c6916ce24319b2b118fbd56a
ShadowsCrypto <ceo@opendapps.cloud>
2024-03-21 21:40:38 +0200

    feat: added ownership http service and removed redundant services

--- src/lib/service/http-services.container.ts
+++ src/lib/service/http-services.container.ts
@@ -5,17 +5,13 @@ import {
   IndexingHttpService,
   MultiSignProposalHttpService,
   NftProxyHttpService,
+  OwnershipHttpService,
   StatsHttpService,
   TokenAsAServiceDeployerHttpService
 } from "@unleashed-business/opendapps-cloud-ts-commons";
 import { Inject, Injectable } from "@angular/core";
 import { BACKEND_BASE_URL_DI_TOKEN } from "../ng-commons.const";
-import { PresaleServiceDeployerHttpService } from "@unleashed-business/opendapps-cloud-ts-commons/dist/web2/presale-service-deployer-http.service";
 import { BlocktimeHttpService } from "@unleashed-business/opendapps-cloud-ts-commons/dist/web2/blocktime-http.service";
-import { StakingAsAServiceDeployerHttpService } from "@unleashed-business/opendapps-cloud-ts-commons/dist/web2/staking-as-a-service-deployer-http.service";
-import {
-  TreasuryDeployerHttpService
-} from "@unleashed-business/opendapps-cloud-ts-commons/dist/web2/treasury-deployer-http.service";
 
 @Injectable({
   providedIn: "root"
@@ -29,11 +25,9 @@ export class NgHttpServicesContainer extends HttpServicesContainer {
       new MultiSignProposalHttpService(baseUrl),
       new IndexingHttpService(baseUrl),
       new DeploymentHttpService(baseUrl),
-      new PresaleServiceDeployerHttpService(baseUrl),
       new BlocktimeHttpService(baseUrl),
-      new StakingAsAServiceDeployerHttpService(baseUrl),
       new StatsHttpService(baseUrl),
-      new TreasuryDeployerHttpService(baseUrl),
+      new OwnershipHttpService(baseUrl),
     );
   }
 }
